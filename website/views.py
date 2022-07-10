318d8a9320cce87a53af823cb366e4bf6f98bf91
Jacob Wolf <jaketwolf1113@gmail.com>
2022-07-10 18:47:55 -0500

    small change

--- website/views.py
+++ website/views.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Blueprint, render_template, request, flash, jsonify, sessions, redirect, abort
 from flask_login import login_required, current_user
 from importlib_metadata import method_cache
