9637ef8454a320ab87d6542e715d97ab4dedca7a
Jacob Wolf <jaketwolf1113@gmail.com>
2022-07-12 20:56:32 -0500

    Updates to Settings Page
    Added Show/Hide Settings Functionality
    Fixed Navbar typo
    Styled Settings
    Styled Navbar
    Added color settings to database

--- website/static/settings.js
+++ website/static/settings.js
@@ -1,3 +1,20 @@
-function toggleAppearance(){
-    
+let root = document.documentElement;
+
+function toggleAppearance(varName){
+    var element = document.getElementById(varName);
+    if (root.style.getPropertyValue('--'+varName) == 'none'){
+        root.style.setProperty('--'+varName, 'block');
+        element.innerHTML = 'Hide';
+    }
+    else if (root.style.getPropertyValue('--'+varName) == 'block'){
+        root.style.setProperty('--'+varName, 'none');
+        element.innerHTML = 'Show';
+    }
+    else if (root.style.getPropertyValue('--'+varName) == ''){
+        root.style.setProperty('--'+varName, 'block');
+        element.innerHTML = 'Hide';
+    }
 }
+
+
+
