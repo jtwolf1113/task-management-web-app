665f1fe75c9333815bfd190e9f9b432e0532e474
Jacob Wolf <jaketwolf1113@gmail.com>
2022-07-25 01:53:36 -0500

    updated dashboard conditions

--- website/static/dashboard.js
+++ website/static/dashboard.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
+    //update every 60 seconds
     var today = new Date();
 
     var date = (today.getMonth()+1)+" "+today.getDate()+", "+today.getHours()+":"+today.getMinutes();
