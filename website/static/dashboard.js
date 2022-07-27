65f44aba6ff7d35bb1817d5fe387ad5ffcc9fe1d
Jacob Wolf <jaketwolf1113@gmail.com>
2022-07-27 01:16:31 -0500

    Minor updates
    Fixed Dashboard Current Date display
    Fixed Overflow on Dashboard
    Added Better Focus Timer Options

--- website/static/dashboard.js
+++ website/static/dashboard.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
     //update every 60 seconds
-    var today = new Date();
-
-    var date = (today.getMonth()+1)+" "+today.getDate()+", "+today.getHours()+":"+today.getMinutes();
-
+    var today = new Date()
+    var date = (today.getMonth()+1).toString()+"/"+today.getDate().toString().padStart(2, '0')+", "+today.getHours().toString().padStart(2, '0')+":"+today.getMinutes().toString().padStart(2, '0')+":"+today.getSeconds().toString().padStart(2, '0');
     document.querySelector("h3#current-time").innerHTML = date;
     });
+
+var clock_display_calculation = setInterval(function(){
+    var today = new Date()
+    var date = (today.getMonth()+1).toString()+"/"+today.getDate().toString().padStart(2, '0')+", "+today.getHours().toString().padStart(2, '0')+":"+today.getMinutes().toString().padStart(2, '0')+":"+today.getSeconds().toString().padStart(2, '0');
+    document.querySelector("h3#current-time").innerHTML = date;
+}, 100); 
+
+
+
